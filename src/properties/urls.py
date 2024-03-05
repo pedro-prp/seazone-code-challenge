5ee70f9b9dcbd3223d120eed26b1282e419f19c0
Pedro Rodrigues <peddroprp@gmail.com>
2024-03-05 17:23:01 -0300

    Implement Property detail view

--- src/properties/urls.py
+++ src/properties/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PropertyListCreateView
+from .views import PropertyListCreateView, PropertyDetailView
 
 urlpatterns = [
     path("", PropertyListCreateView.as_view(), name="property-list-create"),
+    path("<str:pk>/", PropertyDetailView.as_view(), name="property-detail"),
 ]
--- src/properties/views.py
+++ src/properties/views.py
@@ -18,3 +18,44 @@ class PropertyListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PropertyDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            property = Property.objects.get(cod_property=pk)
+        except Property.DoesNotExist:
+            return Response(
+                {"message": "Property not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = PropertySerializer(property)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        try:
+            property = Property.objects.get(cod_property=pk)
+        except Property.DoesNotExist:
+            return Response(
+                {"message": "Property not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = PropertySerializer(property, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            property = Property.objects.get(cod_property=pk)
+        except Property.DoesNotExist:
+            return Response(
+                {"message": "Property not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        property.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
