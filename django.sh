ef1cef6bbdad9c9e7ea24748559a022505ecac1b
Pedro Rodrigues <peddroprp@gmail.com>
2024-03-06 12:58:18 -0300

    Add create superuser in django entrypoint

--- django.sh
+++ django.sh
@@ -2,12 +2,23 @@
 echo "Creating Migrations..."
 python ./src/manage.py makemigrations
 python ./src/manage.py makemigrations properties
-echo ====================================
+echo "===================================="
 
 echo "Starting Migrations..."
 python ./src/manage.py migrate
 python ./src/manage.py migrate properties
-echo ====================================
+echo "===================================="
+
+echo "Checking if Super User Exists..."
+user_exists=$(python ./src/manage.py shell -i python -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin').exists())")
+
+if [ "$user_exists" = "False" ]; then
+    echo "Creating Super User..."
+    python ./src/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+else
+    echo "Super User 'admin' already exists, skipping creation."
+fi
+echo "===================================="
 
 echo "Starting Server..."
 python ./src/manage.py runserver 0.0.0.0:8000
